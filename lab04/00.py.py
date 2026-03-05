c94f8b80dcd5159437eb672ea050c5824a4c64d4
Ivan57-dot <ivanshakur1108@gmail.com>
2026-03-05 13:05:53 +0500

    Update 00.py.py

--- lab04/00.py.py
+++ lab04/00.py.py
@@ -1,6 +1,6 @@
-def make_collector():
+def make_zamikanie():
     items = []
-    def collector(x):
+    def zamikanie(x):
         if x == "стоп":
             result = items.copy()
             items.clear()
@@ -8,13 +8,14 @@ def make_collector():
         else:
             items.append(x)
     
-    return collector
+    return zamikanie
 
-c = make_collector()
+c = make_zamikanie()
 c(1)
 c(2)
 c(3)
 print(c("стоп"))
 c(4)
 c(5)
+
 print(c("стоп"))
