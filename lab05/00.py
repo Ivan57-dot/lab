5fa7699f6c694d47cfd64433039fddfd0297f908
Ivan57-dot <ivanshakur1108@gmail.com>
2026-03-12 14:07:42 +0500

    Update 00.py

--- lab05/00.py
+++ lab05/00.py
@@ -1,22 +1,16 @@
 import os
 
 def find_files(path):
-    count = 0
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.txt'):
                 yield os.path.join(root, file)
-                count += 1
-                if count >= 3:
-                    return
-
+                
 files = list(find_files('.'))
 print("Найдено:", [os.path.basename(f) for f in files])
 
 big_files = list(filter(lambda f: os.path.getsize(f) > 100, files))
-print("Большие (>100 байт):", [os.path.basename(f) for f in big_files])
-
-sizes = list(map(os.path.getsize, files))  
+print("Большие (>100 байт):", [os.path.basename(f) for f in big_files]) 
 
 for i, f in enumerate(files, 1):
     os.rename(f, os.path.join(os.path.dirname(f), f"file_{i}.txt"))
