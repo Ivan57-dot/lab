178de8b716c9b661db83b5cd64abe09317306b50
Ivan57-dot <ivanshakur1108@gmail.com>
2026-04-22 23:27:40 +0500

    Update 03.py

--- lab02/03.py
+++ lab02/03.py
@@ -3,7 +3,7 @@ def count_divisors(n):
         return 1
     count = 2
     for i in range(2, int(n**0.5) + 1):
-        if n % 1 == 0:
+        if n % i == 0:
             if i == n // i:
                 count += 1
             else:
