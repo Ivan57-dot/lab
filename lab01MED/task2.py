13ea0df362611de805bac81c723319fd41aea510
Ivan57-dot <ivanshakur1108@gmail.com>
2026-04-22 23:25:21 +0500

    Add files via upload

---
+++ lab01MED/task2.py
@@ -0,0 +1,13 @@
+def run():
+    r = 42
+    pi = 3.1415926
+    s = pi * (r ** 2)
+    print(round(s, 4))
+    
+    point_1 = (23, 34)
+    d1 = (point_1[0] ** 2 + point_1[1] ** 2) ** 0.5
+    print(d1 > r)
+    
+    point_2 = (30, 30)
+    d2 = (point_2[0] ** 2 + point_2[1] ** 2) ** 0.5
+    print(d1 < r)
